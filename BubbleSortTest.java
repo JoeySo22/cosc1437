5abedfd10cce69a047b290ae540d4a0875f7e158
kingchocomilk <gamereg911@gmail.com>
2017-10-01 03:01:40 -0600

    Updated with Selection Sort

--- BubbleSortTest.java
+++ BubbleSortTest.java
@@ -2,13 +2,13 @@ public class BubbleSortTest
 {
 	public static void main (String[] args)
 	{
-		int[] values = {5,1,32,65,7,4,23};
+		int[] values = {5,1,32,65,7,4,-4,23};
 	
 		System.out.println("Original Order");
 		for (int element : values)
-		System.out.print(element + " ");
+			System.out.print(element + " ");
 
-		bubbleSort(values);
+		selectionSort(values);
 
 		System.out.println("\nSorted Order: ");
 		for (int element : values)
@@ -33,6 +33,47 @@ public class BubbleSortTest
 					array[index] = array[index + 1];
 					array[index + 1] = temp;
 				}
+			}
+		}
+	}
+
+	//private static int findLowestNumber(int[] array)
+	//{
+	//	int lowest = Integer.MAX_VALUE;
+	//	for (int x = 0;
+	//	
+	//}
+
+	private static void selectionSort(int[] array)
+	{
+		int first_index = 0;
+		int lowest = Integer.MAX_VALUE;
+		int lowest_index = 0;
+		int temp = 0;
+
+		while (first_index != array.length + 1)
+		{
+			for (int x = first_index; x < array.length; x++)
+			{
+				if ( array[x] < lowest )
+				{
+					lowest = array[x];
+					lowest_index = x;
+				}
+			}
+			if (lowest != Integer.MAX_VALUE)
+			{
+				temp = array[first_index];
+				array[first_index] = lowest;
+				array[lowest_index] = temp;
+				lowest = Integer.MAX_VALUE;
+				lowest_index = 0;
+				first_index++;
+			}
+			else 
+			{
+				first_index++;
+				continue;
 			}
 		}
 	}
---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test
+{
+	public static void main(String[] args)
+	{
+		String cheese = "A String";
+		System.out.println(cheese);
+		cheese = cheese.substring(3);
+		System.out.println(cheese);
+	}
+}
